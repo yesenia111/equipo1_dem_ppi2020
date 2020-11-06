459752f08c64b7b54cc66a07c20720528605ed67
Alejandro Rodríguez Molina <66125226+Caponauta@users.noreply.github.com>
2020-11-06 12:28:48 -0500

    Add files via upload

--- src/Components/modProducto.js
+++ src/Components/modProducto.js
@@ -21,9 +21,45 @@ class EditarInformacionP extends React.Component {
                   <br />
                   <textarea
                     class="form-control"
-                    id="exampleFormControlTextarea1"                  
-                    rows="8"
+                    id="exampleFormControlTextarea1"
+                    rows="2"
+                    placeholder="ingresa detalles del producto"
                   ></textarea>
+<br/>
+
+
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked/>
+  <label class="form-check-label" for="exampleRadios1">
+    Producto disponible
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
+  <label class="form-check-label" for="exampleRadios2">
+    Producto agotado
+  </label>
+  </div><br/>
+
+                  <form>
+                  <div class="row">
+                  <label for="inputEmail4">Pasillo</label>
+    <div class="col">
+      <input type="text" class="form-control" placeholder="sección"/>
+    </div>
+    </div>
+    <br/>
+                    <div class="form-group">
+                      <label for="exampleFormControlFile1">
+                        Selecciona la imagén
+                      </label>
+                      <input
+                        type="file"
+                        class="form-control-file"
+                        id="exampleFormControlFile1"
+                      />
+                    </div>
+                  </form>
                   <Link to="tendero">
                     <button type="button" class="btn" id="delete">
                       Enviar
